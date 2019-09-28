e6d91ef0eb5d514ac1537a23034f97eb58924a65
Bjorn Reppen <bjornreppen@users.noreply.github.com>
2019-09-28 19:19:24 +0200

    Deploy only data files

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,6 +2,7 @@
 DESTPATH=/home/grunnkart/
 echo Making archive...
 rm naturvern-kart.tar.gz
+find build/ -name "*.schema.*" -type f -delete  
 tar -cjf naturvern-kart.tar.gz -C ./build .
 echo Deploying...
 scp naturvern-kart.tar.gz grunnkart@nin.test.artsdatabanken.no:~/
